28613adfe982778d1ba27b93aae563c41530f94a
221rdb087 <125808567+221rdb087@users.noreply.github.com>
2023-03-28 14:03:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 # python3
 
-def parallel_processing(n, m):
-    assigned_jobs = [[] for _ in range(n)]
-    threads = [(i, 0) for i in range(n)]
-    for job in m:
-        thread_id, completion_time = min(threads, key=lambda x: (x[1], x[0]))
-        assigned_jobs[thread_id].append(completion_time)
-        threads[thread_id] = (thread_id, completion_time + job)
-    return assigned_jobs
+def parallel_processing(n, m, data):
+    output = []
+    threads = [(i, 0) for i in range(n)] 
+    for i in range(m):
+        thread = min(threads, key=lambda x: x[1]) 
+        output.append((i, thread[0])) 
+        threads[thread[0]] = (thread[0], thread[1] + data[i]) 
+    return output
+
 
 def main():
+   
     n, m = map(int, input().split())
-    m = list(map(int, input().split()))
-    result = parallel_processing(n, m)
-    for times in result:
-        for time in times:
-            print(time, end=' ')
-        print()
-
+    data = list(map(int, input().split()))
+    result = parallel_processing(n, m, data)
+    for pair in result:
+        print(pair[0], pair[1])
 
 if __name__ == "__main__":
     main()
