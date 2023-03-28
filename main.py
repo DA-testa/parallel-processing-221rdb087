4f404867edfb728a84d5c68f11e5e1dd35475e0b
221rdb087 <125808567+221rdb087@users.noreply.github.com>
2023-03-28 14:07:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# python3
-
+# python 3
+import heapq
 def parallel_processing(n, m, data):
     output = []
-    threads = [(i, 0) for i in range(n)] 
+    threads = [(i, 0) for i in range(n)]
+    heapq.heapify(threads)
     for i in range(m):
-        thread = min(threads, key=lambda x: x[1]) 
-        output.append((i, thread[0])) 
-        threads[thread[0]] = (thread[0], thread[1] + data[i]) 
+        thread = heapq.heappop(threads)
+        output.append((thread[0], thread[1]))
+        heapq.heappush(threads, (thread[0], thread[1] + data[i]))
     return output
 
-
 def main():
-   
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
     result = parallel_processing(n, m, data)
